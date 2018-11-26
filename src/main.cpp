16a4880e533da471100b343ac2db08505e83ee7a
Gabriel Goffart <gofg2301@usherbrooke.ca>
2018-11-26 12:20:04 -0500

    code for presentation

--- src/main.cpp
+++ src/main.cpp
@@ -1,243 +1,371 @@
 #include <LibRobus.h>
+#include <MFRC522.h>
+#include <SPI.h>
+#include <LiquidCrystal.h>
 
-void ResetEncodeurs();
-int CalculerErreurDroite();
-void Avancer();
-void Avancer(const int TEMPS);
-void Reculer();
-void Reculer(const int TEMPS);
-void Tourner(const int TEMPS);
-void Tourner(const int MS, const int MOTEUR);
+//Variable contenant les commandes des clients
+String commandeActuelle = "";
 
-bool Son();
-void Random();
-void Pattern();
-// void Avancer(int pulse);
-// void DemiTour();
-enum choix {
-    AVANCER = 1,
-    RECULER = 2,
-    GAUCHE = 3,
-    DROITE = 4
+//Variables pour le deplacement
+float vTarget = 0.20; // vitesse initial du robot
+float vMax = vTarget + 0.02;
+float vMin = vTarget - 0.02;
+int positionRobot = 0; // position de depart du robot
+float ACCEL = 0.01; //acceleration du robot
+bool isScanning = true; 
+
+//Variables des capteurs
+int gauche = 0;
+int milieu = 0;
+int droite = 0;
+
+//Define des capteurs
+#define GAUCHEAVANT 35  //capteur de gauche en avant
+#define MILIEUAVANT 33  //capteur du milieu en avant
+#define DROITEAVANT 37  //capteur de droite en avant
+#define GAUCHEARRIERE 41    //capteur de gauche en arriere
+#define MILIEUARRIERE 43    //capteur du milieu en arriere
+#define DROITEARRIERE 39    //capteur de droite en arriere
+
+//TODO: changer les pins
+//Define des LED
+#define LEDC1 45    //LED du contenant1
+#define LEDC2 47    //LED du contenant2 
+#define LEDC3 49    //LED du contenant3
+
+//Define des pompes
+#define POMPE_BLEU 44    //Pompe bleu
+#define POMPE_ROUGE 45    //Pompe rouge
+#define POMPE_JAUNE 46   //Pompe jaune
+#define PWM 100     //pwm sur les pompes
+
+//Define de l'ecran LCD et RFID
+#define RS 13
+#define EN 11
+#define D4 7
+#define D5 4
+#define D6 3
+#define D7 2
+
+#define RST_PIN 9
+#define SS_PIN  53
+
+
+//Define angle du depart
+#define INITIAL 180   //angle initial
+#define SERVIR  0     //angle pour la position des pompes
+
+//Variables pour l'ecran LCD et RFID
+LiquidCrystal lcd(RS, EN, D4, D5, D6, D7);
+MFRC522 rfid(SS_PIN, RST_PIN);
+bool paiementNonEffectuee = true;
+
+byte nuidPICC[4];
+
+//Structure de la commande
+struct Commande
+{
+    int position = 0;
+    int couleur = 0;
 };
 
+//Structure des contenants
+struct Contenants
+{
+    float quantiteC1 = 0;
+    float quantiteC2 = 0;
+    float quantiteC3 = 0;
+};
 
-//poser une vitesse x pour moteur 0 et 1
-//0.6
-const float vTarget = 0.8;
+//Variables des contenants
+struct Contenants contenantUtilise;
 
-//tier de vTarget
-const float vDemiVitesse = vTarget / 3;
-const float ACCEL = 0.2;
-float vGauche = 0;
-float vDroite = vGauche;
+const int couleurs [6][2] = {
+    {POMPE_BLEU, POMPE_BLEU},
+    {POMPE_ROUGE, POMPE_ROUGE},
+    {POMPE_JAUNE, POMPE_JAUNE},
+    {POMPE_ROUGE, POMPE_JAUNE},
+    {POMPE_BLEU, POMPE_JAUNE},
+    {POMPE_BLEU, POMPE_ROUGE}
+};
 
-//Variables pour le PID
-const float KP = 0.001; //IDEAL : 0.001
-const float KI = 0.0001; //IDEAL : 0.0001
-int erreurDroite = 0;
-int erreurTotale = 0;
-int tempsCycle = 150; //en ms
+//Fonctions prototypes
+void read_Bluetooth();
+bool VerificationContenants();
+void UpdateContenants(const int couleur);
+struct Commande SeparationCommande(String commandeActuelle);
+void Deplacement(const int positionClient);
+void EffectuerCommande(struct Commande commande);
+void FaireDrink(const int couleur);
+void resetContenants();
+bool paiementRFID();
 
-//Variables pour la fonction random 
-//(ajustements pour un peu moins de random)
-int lastRand = 1;
-int nbA = 0;
-int nbR = 0;
-
-void setup() {
+void setup() 
+{
     BoardInit();
-    delay(5000);
     Serial.begin(9600);
-    Pattern();
-    //MOTOR_SetSpeed(LEFT, 0);
-    //MOTOR_SetSpeed(RIGHT, 0);
+    BluetoothInit();
+    BLUETOOTH_setCallback(read_Bluetooth);
+    pinMode(GAUCHEAVANT, INPUT); //gauche
+    pinMode(MILIEUAVANT, INPUT); //milieu
+    pinMode(DROITEAVANT, INPUT); //droite
+    pinMode(GAUCHEARRIERE, INPUT); //gauche-arriere
+    pinMode(MILIEUARRIERE, INPUT); //milieu-arriere
+    pinMode(DROITEARRIERE, INPUT); //droite-arriere
+    // pinMode(LEDC1, OUTPUT); //LED contenant1
+    // pinMode(LEDC2, OUTPUT); //LED contenant2
+    // pinMode(LEDC3, OUTPUT); //LED contenant3
+    pinMode(POMPE_BLEU, OUTPUT);    //pompe contenant1
+    pinMode(POMPE_ROUGE, OUTPUT);    //pompe contenant2  
+    pinMode(POMPE_JAUNE, OUTPUT);    //pompe contenant3
+    // lcd.begin(16, 2);
+    // SPI.begin();
+    // rfid.PCD_Init();
+    SERVO_Enable(SERVO_1); //servomoteur
+    SERVO_SetAngle(SERVO_1, INITIAL);
+    delay(1000);
+    SERVO_Disable(SERVO_1);
 }
 
-void loop() {
-    Random();
+
+void loop()
+{
+    if(ROBUS_IsBumper(FRONT)) {
+        resetContenants();
+    }
+
+    if (!VerificationContenants())
+    {
+        Deplacement(0);
+        return;
+    }
+
+    if (commandeActuelle.length() < 3)
+    {
+        delay(200);
+        return;
+    }
+
+    struct Commande commande = SeparationCommande(commandeActuelle);
+    Deplacement(commande.position);
+
+    // while (paiementNonEffectuee)
+    // {
+    //     paiementNonEffectuee = paiementRFID();
+    // }
+    // EffectuerCommande(commande);
 }
 
-void Random() {
-    int random = 0;
-    int temps = 0;
-    
-    //trouver une valeur random à exécuter.
-        //si le dernier choix était tourner, on va choisir avancer/reculer
-        //si le dernier choix était avancer/reculer, on va choisir tourner
-    do {
-        random = (rand() % 2) + 1;
-        temps = ((rand() % 2) + 1 )* 1000;
+void read_Bluetooth() //assigne l'information reçu de l'application
+{
+    commandeActuelle = "";
+    while(commandeActuelle.length() < 3) 
+    {
+        commandeActuelle += BLUETOOTH_read();
+        //Serial.println(commandeActuelle);
+    }
+}
 
-        if(lastRand < 3)
-            random += 2;
+bool VerificationContenants() //LED d'un contenant s'allume s'il reste juste assez de liquide pour une consommation (quand il reste 30 ml)
+{
+    if (contenantUtilise.quantiteC1 > 3) 
+    {
+        digitalWrite(LEDC1, HIGH);
+        return false;
+    }
+    if (contenantUtilise.quantiteC2 > 3)
+    {
+        digitalWrite(LEDC2, HIGH);
+        return false;
+    }
+    if (contenantUtilise.quantiteC3 > 3)
+    {
+        digitalWrite(LEDC3, HIGH);
+        return false;
+    }
 
-    }while(random == lastRand);
+    digitalWrite(LEDC1, LOW);
+    digitalWrite(LEDC2, LOW);
+    digitalWrite(LEDC3, LOW);
+    return true;
+}
 
-    lastRand = random;
+void resetContenants() {
+    contenantUtilise.quantiteC1 = 0;
+    contenantUtilise.quantiteC2 = 0;
+    contenantUtilise.quantiteC3 = 0;
+}
 
-    switch(random) {
-        //AVANCER
-        case choix::AVANCER: 
-            if(nbA > 2) {
-                Reculer(temps);
-                nbA = 0;
-                return;
-            }
-            Avancer(temps);
-            nbA++; 
-            nbR = 0; 
+void UpdateContenants(const int couleur)
+{
+    switch(couleur)
+    {
+        case 1:
+            contenantUtilise.quantiteC1++;
             break;
-        //RECULER
-        case choix::RECULER: 
-            if(nbR > 2) {
-                Avancer(temps);
-                nbR = 0;
-                return;
-            }
-            Reculer(temps);
-            nbR++; 
-            nbA = 0; 
+        case 2:
+            contenantUtilise.quantiteC2++;
             break;
-        //TOURNER GAUCHE
-        case choix::GAUCHE: Tourner(temps/8, LEFT); break;
-        //TOURNER DROITE
-        case choix::DROITE: Tourner(temps/8, RIGHT); break;
-        //shouldnt be an issue
+        case 3:
+            contenantUtilise.quantiteC3++;
+            break;
+        case 4:
+            contenantUtilise.quantiteC2+= 0.5;
+            contenantUtilise.quantiteC3+= 0.5;
+            break;
+        case 5:
+            contenantUtilise.quantiteC1+= 0.5;
+            contenantUtilise.quantiteC3+= 0.5;
+            break;
+        case 6:
+            contenantUtilise.quantiteC1+= 0.5;
+            contenantUtilise.quantiteC2+= 0.5;
+            break;
         default: break;
     }
-
 }
 
-void Pattern() {
-    Avancer(600);
-    Tourner(tempsCycle, LEFT);
-    Avancer(600);
-    Tourner(tempsCycle, RIGHT);
-    Avancer(2500);
-    Tourner(tempsCycle * 2, LEFT);
-    Avancer(1500);
+struct Commande SeparationCommande(String commande) //Separe la position et la couleur de la String de la commande
+{
+    struct Commande commandeDesire;
+    int index = commande.indexOf(',');
+
+    commandeDesire.position = commande.substring(0,index).toInt();
+    commandeDesire.couleur = commande.substring(index + 1).toInt();
+
+    return commandeDesire;
 }
 
-void Tourner(const int MS, const int MOTEUR) {
-    int vitesse = vGauche < 0 ? vTarget: -vTarget;
-    
-    MOTOR_SetSpeed(MOTEUR, vitesse);
+void Deplacement(int positionClient) //Deplacement du robot
+{
+    // Pars de sa position de depart jusqu'à la position du client à l'aide du suiveur de lignes.
+    int NbrLignes = positionClient - positionRobot;
+    float speed = vTarget;
+    int sens;
+    int motor;
 
-    for(int i = 0; i < MS; i += tempsCycle) {
-        if(Son()) {
-            MOTOR_SetSpeed(LEFT, 0);
-            MOTOR_SetSpeed(RIGHT, 0);
-            delay(10000);
-            MOTOR_SetSpeed(MOTEUR, vitesse);
-            MOTOR_SetSpeed(!MOTEUR, -vitesse);
-        } else {
-            delay(100);
+    if (NbrLignes == 0)
+    {
+        commandeActuelle = "";
+        Serial.println("Position deja atteinte");
+        return; //le robot est deja au bon endroit, servir le drink;
+    }
+
+    if (NbrLignes > 0)
+    {
+        gauche = GAUCHEAVANT;
+        milieu = MILIEUAVANT;
+        droite = DROITEAVANT;
+        motor = RIGHT;
+        sens = 1;
+    }
+    else
+    {
+        gauche = GAUCHEARRIERE;
+        milieu = MILIEUARRIERE;
+        droite = DROITEARRIERE;
+        motor = LEFT;
+        sens = -1;
+    }
+
+    MOTOR_SetSpeed(RIGHT, (sens * vTarget));
+    MOTOR_SetSpeed(LEFT, (sens * vTarget));
+
+    Serial.println(positionClient);
+    Serial.println(positionRobot);
+    Serial.println();
+    while(positionRobot != positionClient) { 
+        delay(100);
+
+        if (!digitalRead(GAUCHEAVANT) && !digitalRead(MILIEUAVANT) && !digitalRead(DROITEAVANT)) {
+            if (!isScanning) {
+                isScanning = true;
+                positionRobot += sens;
+                Serial.println(positionRobot);
+            }
+        }
+        else {
+            isScanning = false;
+            if(!digitalRead(gauche)) {
+                if(speed < vMax) {
+                    speed += ACCEL;
+                }
+            }                
+            else if(!digitalRead(droite)) {
+                if (speed > vMin) {
+                    speed -= ACCEL;
+                    Serial.println(speed);
+                }
+            }
+            else if (!digitalRead(milieu)) {
+                speed = vTarget;
+            }
+            MOTOR_SetSpeed(motor, (speed*sens));
+            MOTOR_SetSpeed(!motor, (vTarget*sens));
         }
     }
-
-    MOTOR_SetSpeed(MOTEUR,-vitesse);
-
-    delay(tempsCycle);
-    ResetEncodeurs();
+    Serial.println("Reussis");
+    commandeActuelle = "";
+    MOTOR_SetSpeed(LEFT, 0);
+    MOTOR_SetSpeed(RIGHT, 0);
 }
 
-void Avancer(){
-
-    ResetEncodeurs();
-
-    if(vGauche < vTarget) {
-        vGauche += ACCEL;
-        vDroite += ACCEL;
+bool paiementRFID() //Retourne vrai si le paiment n'est pas effectuee et faux s'il est fait
+{
+    lcd.setCursor(0, 0);
+    lcd.print("Paiement : 2,50$");
+    if ( ! rfid.PICC_IsNewCardPresent()) 
+    {
+        return true;
     }
-    //SET VITESSE
-    MOTOR_SetSpeed(LEFT, vGauche);
-    MOTOR_SetSpeed(RIGHT, vDroite);
-    
-    //ATTEND UN CYCLE
-    delay(100);
-    if(Son()) {
-        MOTOR_SetSpeed(RIGHT, 0);
-        MOTOR_SetSpeed(LEFT, 0);
-        delay(10000);
-        vGauche = 0;
-        vDroite = 0;
+    if ( ! rfid.PICC_ReadCardSerial()) 
+    {
+        return true;
     }
-    //CALCULE L'ERREUR DE DROITE VS GAUCHE 
-    erreurDroite = CalculerErreurDroite();
-    erreurTotale += erreurDroite;
-
-    //CHANGE LA VITESSE PAR RAPPORT À L'ERREUR
-    vDroite += (erreurDroite * KP) + (erreurTotale * KI) ;
-
-}
-
-void Avancer(const int TEMPS) {
-    for(int tps = 0;tps < TEMPS; tps += tempsCycle) {
-        Avancer();
-    }
-} 
-
-void Reculer(){
-
-    ResetEncodeurs();
-
-    if(vGauche > -vTarget) {
-        vGauche -= ACCEL;
-        vDroite -= ACCEL;
-    }
-    //SET VITESSE
-    MOTOR_SetSpeed(LEFT, vGauche);
-    MOTOR_SetSpeed(RIGHT, vDroite);
-    
-    //ATTEND UN CYCLE
-    delay(100);
-    if(Son()) {
-        MOTOR_SetSpeed(RIGHT, 0);
-        MOTOR_SetSpeed(LEFT, 0);
-        delay(10000);
-        vGauche = 0;
-        vDroite = 0;
-    }
-    //CALCULE L'ERREUR DE DROITE VS GAUCHE 
-    erreurDroite = CalculerErreurDroite();
-    erreurTotale += erreurDroite;
-
-    //CHANGE LA VITESSE PAR RAPPORT À L'ERREUR
-    vDroite += (erreurDroite * KP) + (erreurTotale * KI) ;
-
-    if(Son()) {
-        MOTOR_SetSpeed(RIGHT, 0);
-        MOTOR_SetSpeed(LEFT, 0);
-        delay(10000);
-        vGauche = 0;
-        vDroite = 0;
-    }
-}
-
-void Reculer(const int TEMPS) {
-    for(int tps = 0; tps < TEMPS; tps += tempsCycle) {
-        Reculer();
-    }
-}
-
-void ResetEncodeurs(){
-    ENCODER_Reset(LEFT);
-    ENCODER_Reset(RIGHT);
-}
-
-int CalculerErreurDroite() {
-    return ENCODER_Read(LEFT) - ENCODER_Read(RIGHT); 
-}
-
-bool Son() {
-    for(int i =0; i< 4; i++){
-        bool isHigh = ( analogRead(A0) * (5/1023.0) ) > 2.8;
-        if(!isHigh) {
-            return false;
-        }
-
-        delay(50);
+    if (rfid.uid.uidByte[0] != nuidPICC[0] || rfid.uid.uidByte[1] != nuidPICC[1] ||
+        rfid.uid.uidByte[2] != nuidPICC[2] || rfid.uid.uidByte[3] != nuidPICC[3] ) 
+    {
+        lcd.clear();
+        lcd.setCursor(0, 0);
+        lcd.print("Transaction...");
+        delay(1000);
+        lcd.clear();
+        lcd.setCursor(0, 0);
+        lcd.print("Paiment Recu!");
+        delay(1000);
+        return false;
     }
     return true;
+}
+
+void EffectuerCommande(struct Commande commande)
+{
+    UpdateContenants(commande.couleur);
+
+    SERVO_Enable(SERVO_1);
+    SERVO_SetAngle(SERVO_1, SERVIR);
+    delay(1000);
+    SERVO_Disable(SERVO_1);
+
+    FaireDrink(commande.couleur);
+    
+    SERVO_Enable(SERVO_1);
+    SERVO_SetAngle(SERVO_1, INITIAL);
+    delay(1000);
+    SERVO_Disable(SERVO_1);
+}
+
+void FaireDrink(const int couleur) //Bouge le servomoteur et active les pompes pour faire le drink
+{
+    const int* pompes = couleurs[couleur];
+    uint8_t pompe;
+    for(int i = 0; i < 2; i++) {
+
+        pompe = uint8_t(pompes + i);
+
+        analogWrite(pompe,PWM);
+        delay(700);
+        analogWrite(pompe, 0);
+        delay(1000);
+    }
 }
